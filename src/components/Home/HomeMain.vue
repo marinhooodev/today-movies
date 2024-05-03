fcdbc0089cc1388c337fca872a64d238e5c2d2be
Gabriel Marinho <gabriel.anjo722@gmail.com>
2024-05-03 16:02:36 -0300

    feat: 'didnt find' section

--- src/components/Home/HomeMain.vue
+++ src/components/Home/HomeMain.vue
@@ -1,9 +1,11 @@
 <template>
   <HomeBanner />
   <DayTrendingMovies />
+  <DidntFind />
 </template>
 
 <script setup lang="ts">
+import DidntFind from "./DidntFind.vue";
 import DayTrendingMovies from "./DayTrendingMovies.vue";
 import HomeBanner from "./HomeBanner.vue";
 </script>
